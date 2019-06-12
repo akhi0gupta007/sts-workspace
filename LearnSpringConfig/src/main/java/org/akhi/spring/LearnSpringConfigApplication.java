7b3e03ffdd3150055e31739b4851981c30def32f
Akhilesh <akhi0gupta007@gmail.com>
2019-06-13 00:57:11 +0530

    hysterix fault tolerence

--- LearnSpringConfig/src/main/java/org/akhi/spring/LearnSpringConfigApplication.java
+++ LearnSpringConfig/src/main/java/org/akhi/spring/LearnSpringConfigApplication.java
@@ -3,8 +3,10 @@ package org.akhi.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class LearnSpringConfigApplication {
 
 	
--- LearnSpringConfig/src/main/java/org/akhi/spring/web/ConfigTestController.java
+++ LearnSpringConfig/src/main/java/org/akhi/spring/web/ConfigTestController.java
@@ -4,8 +4,11 @@ import org.akhi.spring.config.Configuration;
 import org.akhi.spring.model.ConfigModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RefreshScope
@@ -15,9 +18,20 @@ public class ConfigTestController {
 	@Autowired
 	private Configuration config;
 
-	@RequestMapping("/testConfig")
+	@RequestMapping("/testConfig")	
 	public ConfigModel getConfig(){
 		return new ConfigModel(config.getMaximum(), config.getMinimum());
+	}
+	
+	@HystrixCommand(fallbackMethod = "fallBackMethod")
+	@GetMapping("/fault-example")
+	public ConfigModel retrieveConfig() {
+		throw new RuntimeException("Not available");
+	}
+	
+	
+	public ConfigModel fallBackMethod() {
+		return new ConfigModel(9,999);		
 	}
 
 }
