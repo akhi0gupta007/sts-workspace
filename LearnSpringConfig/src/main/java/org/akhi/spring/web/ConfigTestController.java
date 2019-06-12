6adf372154dc0230bc2ee23a33c76f0a26a2c27b
Akhilesh <akhi0gupta007@gmail.com>
2019-06-12 23:19:11 +0530

    Web service tracking through zipking and spring client refresh

--- LearnSpringConfig/src/main/java/org/akhi/spring/web/ConfigTestController.java
+++ LearnSpringConfig/src/main/java/org/akhi/spring/web/ConfigTestController.java
@@ -3,10 +3,12 @@ package org.akhi.spring.web;
 import org.akhi.spring.config.Configuration;
 import org.akhi.spring.model.ConfigModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigTestController {
 
 
