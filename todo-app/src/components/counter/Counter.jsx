994bd1b3482a03e9562e4d003600c1446b8d7911
Akhilesh <akhi0gupta007@gmail.com>
2019-08-23 02:08:17 +0530

    state in react

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
- function Counter() {
-    
-      return(
-        <div className="counter">       
-          Counter
-        </div>
-      );
-    
-  }
- 
+import React, { Component } from 'react';
+import './Counter.css'
+
+class Counter extends Component {
+    //Define initial state in the constructor
+    // state => counter 0
+
+    constructor() {
+        super(); // this is must if you want to use this
+        this.state = {
+            counter: 0
+        }
+
+        this.increment = this.increment.bind(this)
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <button onClick={this.increment}>+1</button>
+                <span className="count">{this.state.counter}</span>
+            </div>
+        );
+    }
+
+    increment() {
+        console.log('increment');
+        this.setState({
+            counter: this.state.counter + 1
+        }
+        );
+    }
+
+}
+
+
 export default Counter;
