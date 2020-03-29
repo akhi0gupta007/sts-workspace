1093e30b4d86b75bf44f460abfe38c1dd765dedc
Akhilesh <akhi0gupta007@gmail.com>
2020-03-29 18:27:33 +0530

    two way handler

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,16 +9,29 @@ class App extends Component {
       { name: 'Akhilesh' , age:31},
       {name:'Nancy', age:32},
       {name:'Aditi', age:3}
-    ]
+    ],
+    otherStatus:'Guten morgen'
   }
 
-  switchMatchHandler = () => {
-    console.log('Was clicked');
+  switchMatchHandler = (newName) => {
+    console.log('Was clicked'+this.state.persons);
     //DON'T DO THIS this.state.persons[0].name = 'Akhilesh Kumar Gupta';
     this.setState({
       persons: [
-        { name: 'Akhilesh kumar Gupta', age: 31 },
+        { name: newName, age: 31 },
         { name: 'Nancy', age: 32 },
+        { name: 'Aditi', age: 3 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    console.log('Was clicked'+this.state.persons);
+    //DON'T DO THIS this.state.persons[0].name = 'Akhilesh Kumar Gupta';
+    this.setState({
+      persons: [
+        { name: 'Max', age: 31 },
+        { name: event.target.value, age: 32 },
         { name: 'Aditi', age: 3 }
       ]
     })
@@ -28,9 +41,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I am a react App</h1>
-        <button onClick={this.switchMatchHandler}>Switch Name</button>
+        <button onClick={() => this.switchMatchHandler('Akhilesh Gupta')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> </Person>
+        <Person 
+        click={this.switchMatchHandler.bind(this,'Vicky')} 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        changed={this.nameChangeHandler}>
+        </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>  My Hobbies : Racing</Person>
       </div>
     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const person = (props) => {
     return (
     <div>
-         <p>I am a {props.name} and I am {props.age} year old!</p>
+         <p onClick={props.click}>I am a {props.name} and I am {props.age} year old!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
     </div>
     )
 };
