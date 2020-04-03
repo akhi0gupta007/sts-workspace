c1783ddc76cd610a78159ee2de9d093e66534719
Akhilesh <gupta.akhilesh@siemens.com>
2020-04-04 01:55:01 +0530

    css modules

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,34 +1,17 @@
 import React from 'react';
-//import './Person.css';
-import styled from 'styled-components'
+import classes from './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #46bd29;
-    padding: 16px;
-    text-align: center;   
-    
-     @media(min-width:500px){
-            width: 450px;
-    }
-`;
 
 const person = (props) => {
-    // const style = {
-    //     '@media(min-width:500px)': {
-    //         width: '450px'
-    //     }
-    // }
+
 
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <p>I am a {props.name} and I am {props.age} year old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} defaultValue={props.name} />
             <button onClick={props.click}>Delete</button>
-        </StyledDiv>
+        </div>
 
     )
 };
