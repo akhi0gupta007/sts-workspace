d07ab801bc59757b88da91913cb1a02ac2ba650d
Akhilesh <akhi0gupta007@gmail.com>
2020-04-12 02:27:58 +0530

    Intitial burger project setup

---
+++ burger-shop/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from "../../hoc/Auxilary";
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+  <Aux>
+    <div>ToolBar, SideDrawer,Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
+    
