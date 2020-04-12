a1e059a5f8bad8a80895b33bb320f3af167a9e89
Akhilesh <akhi0gupta007@gmail.com>
2020-04-13 04:03:53 +0530

    Burger more button and builder

--- burger-shop/src/components/Burger/Burger.js
+++ burger-shop/src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredient = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>;
-    });
-  });
+  let transformedIngredient = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
   console.log(transformedIngredient);
+  if (transformedIngredient.length === 0) {
+    transformedIngredient = <p>Please start adding ingredients</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
