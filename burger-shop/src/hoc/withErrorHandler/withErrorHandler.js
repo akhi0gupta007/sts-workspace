cf8702c7d72e7f2cd54d72a98355abece5a6401b
Akhilesh <akhi0gupta007@gmail.com>
2020-04-20 05:42:31 +0530

    hoc implementation and eror handling global

--- burger-shop/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-shop/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import Aux from "../Auxilary/Auxilary";
+import Modal from "../../components/UI/Modal/Modal";
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return(
-            <WrappedComponent {...props}/>
-        );
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+    componentDidMount() {
+      axios.interceptors.request.use((request) => {
+        this.setState({ error: null }); //reset the error object
+        return request;
+      });
+      axios.interceptors.response.use(res => res, (error) => {
+        this.setState({ error: error });
+      });
     }
-}
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message:null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
 
 export default withErrorHandler;
