9e51d3dfa13b1f6a1fd7a34028941bf9b8753737
Akhilesh <akhi0gupta007@gmail.com>
2020-04-21 02:27:03 +0530

    router continue

--- posts/src/components/Post/Post.js
+++ posts/src/components/Post/Post.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./Post.css";
 
-import './Post.css';
-
-const post = (props) => (
+const post = (props) => {
+  console.log("[post] props", props);
+  return (
     <article className="Post" onClick={props.clicked}>
-        <h1>{props.title}</h1>
-        <div className="Info">
-            <div className="Author">{props.author}</div>
-        </div>
+      <h1>{props.title}</h1>
+      <div className="Info">
+        <div className="Author">{props.author}</div>
+      </div>
     </article>
-);
-
-export default post;
+  );
+};
+//We wrapped with hoc because we need to access history, location and match property, as its nested componenet within routed component
+export default withRouter(post);
