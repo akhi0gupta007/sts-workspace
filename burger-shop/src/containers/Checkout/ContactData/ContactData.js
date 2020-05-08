61f82aecf84a4b65a2bafcc5c05cd8d5d99f82fd
Akhilesh <akhi0gupta007@gmail.com>
2020-05-09 01:01:47 +0530

    some refractoring

--- burger-shop/src/containers/Checkout/ContactData/ContactData.js
+++ burger-shop/src/containers/Checkout/ContactData/ContactData.js
@@ -114,6 +114,7 @@ class ContactData extends Component {
       ingredients: this.props.ings,
       price: this.props.totalPrice,
       orderData: formData,
+      userId:this.props.userId
     };  
     this.props.onOrderBurger(order,this.props.token); 
   };
@@ -199,7 +200,8 @@ const mapStateToProps= state => {
     ings : state.burgerBuilder.ingredients,
     totalPrice : state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token:state.auth.token
+    token:state.auth.token,
+    userId:state.auth.userId
   };
 }
 
