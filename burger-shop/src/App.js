d116b88440db5a5d16d4024929cad1d7d4655a3c
Akhilesh <akhi0gupta007@gmail.com>
2020-05-09 03:03:42 +0530

    Lazy loading

--- burger-shop/src/App.js
+++ burger-shop/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { Route, Switch ,withRouter,Redirect} from "react-router-dom";
 import { connect } from "react-redux";
+import asyncComponent from './hoc/asynComponent/asyncComponent';
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
+
+const asyncCheckout = asyncComponent(() =>{
+  return import('./containers/Checkout/Checkout'); //custom lazy loaded component, we can do it with other components
+});
 
 class App extends Component {
 
@@ -27,7 +31,7 @@ class App extends Component {
 
   if(this.props.isAuthenticated) {
     routes =   (      <Switch>
-    <Route path="/checkout" component={Checkout} />
+    <Route path="/checkout" component={asyncCheckout} />
     <Route path="/orders" component={Orders} />          
     <Route path="/logout" component={Logout} />
     <Route path="/auth" component={Auth} />
